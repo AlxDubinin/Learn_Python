e94696bf54d56e71e52ac8387f139495c2ed46e2
Alexey <71983378+AlxDubinin@users.noreply.github.com>
2022-01-30 14:01:34 +0300

    Start 01-01.py...

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print('Start learning Python...')
     print('-- Chapter I --')
+    print('Переменные и типы данных - файл Learn_01_01.py')
+
 
 
 # Press the green button in the gutter to run the script.
