c0d55dccbfccbcf077f57b468c896a849ab61e27
Alexey <71983378+AlxDubinin@users.noreply.github.com>
2022-01-30 13:55:46 +0300

    Update main.py
    Add new descriptions...

--- main.py
+++ main.py
@@ -7,10 +7,12 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print('Start learning Python...')
+    print('-- Chapter I --')
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Alex')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
